61de60c26bbd68f08fce8238702239a0317a9ddc
Felipe <felipesakaiipolito@gmail.com>
2024-08-26 08:26:02 -0400

    Add project files.

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritorio";
+
+            byte idade = 30;
+            int codigo = 5290;
+            string genero = "M";
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine($" Produtos: \n {produto1},cujo preço é ${preco1}" +
+                $"\n {produto2}, cujo preço é ${preco2}");
+
+            Console.WriteLine($"Registo: {idade}, codigo {codigo} e genero:{genero}.");
+            
+            Console.WriteLine($"{medida:F8}");
+            Console.WriteLine($"{medida:F3}");
+            Console.WriteLine(medida.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Aula2/Aula2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Contracts;
+using System.Globalization;
+
+Console.WriteLine("ESCREVA SEU NOME: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("ESCREVA QUANTOS QUARTOS TEM NA SUA CASA: ");
+int quartos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ESCREVA O PREÇO DO PRODUTO: ");
+double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.WriteLine("ESCREVA SEU SOBRENOME, IDADE E ALTURA: ");
+string[] pessoa = Console.ReadLine().Split(' ');
+string sobrenome = pessoa[0];
+int idade = int.Parse(pessoa[1]);
+double altura = double.Parse(pessoa[2],CultureInfo.InvariantCulture);
+
+
+
+
+Console.WriteLine(nome);
+Console.WriteLine(quartos);
+Console.WriteLine(preco.ToString("F"));
+Console.WriteLine(sobrenome);
+Console.WriteLine(idade);
+Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
