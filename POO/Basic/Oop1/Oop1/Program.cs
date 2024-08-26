ebca8f3597e04542ed8079f54d475b90c6c1567f
Felipe <felipesakaiipolito@gmail.com>
2024-08-26 17:31:30 -0400

    Usando POO com C#

---
+++ POO/Basic/Oop1/Oop1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Oop1;
+
+Person p1 = new Person();
+Person p2 = new Person();
+
+Console.WriteLine("Digite o nome da primeira Pessoa: ");
+Console.Write("Nome: ");
+p1.nome = Console.ReadLine();
+Console.Write("Idade: ");
+p1.idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o nome da Segunda Pessoa: ");
+Console.Write("Nome: ");
+p2.nome = Console.ReadLine();
+Console.Write("Idade: ");
+p2.idade = int.Parse(Console.ReadLine());
+
+if(p1.idade > p2.idade)
+{
+    Console.WriteLine("Pessoa Mais velha " + p1.nome);
+}
+else
+{
+    Console.WriteLine("Pessoa Mais Velha " + p2.nome);
+}
