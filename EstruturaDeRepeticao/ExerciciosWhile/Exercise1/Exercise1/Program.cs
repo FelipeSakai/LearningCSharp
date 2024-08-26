30bf8cc031b9ca038503209771bcc513f983d091
Felipe <felipesakaiipolito@gmail.com>
2024-08-26 14:27:29 -0400

    Exercise While

---
+++ EstruturaDeRepeticao/ExerciciosWhile/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite a senha do banco");
+int senha = int.Parse(Console.ReadLine());
+
+while (senha != 2003)
+{
+    Console.WriteLine("Senha INVALIDA");
+    senha = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Acesso Permitido");
