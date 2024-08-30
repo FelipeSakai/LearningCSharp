1067d1412de83bc8de7e07b9606c0a54dcaf840a
Felipe <felipesakaiipolito@gmail.com>
2024-08-30 17:13:20 -0400

    Using getter and setter , bankaccount

---
+++ Constructor,Sobrecarga,Encapsulamento/Sobrecarga/Sobrecarga/Bancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class Bancaria
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; private set; }
+
+        public double Saldo { get; private set; }
+
+        public Bancaria(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public Bancaria(int numero, string nome, double saldo) : this(numero, nome)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Nome: "
+                + Nome
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Constructor,Sobrecarga,Encapsulamento/Sobrecarga/Sobrecarga/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Exercise;
+using System.Globalization;
+
+Bancaria conta;
+
+Console.WriteLine("Digite o numero da Conta Bancaria: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o seu nome: ");
+string nome =  Console.ReadLine();
+
+Console.WriteLine("Havera Deposito ? ");
+char resp = char.Parse(Console.ReadLine());
+if (resp == 's' || resp== 'S')
+{
+    Console.WriteLine("Digite o valor para deposito: ");
+    double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+    conta = new Bancaria(numero, nome);
+}
+else
+{
+    conta = new Bancaria(numero, nome);
+}
+
+Console.WriteLine();
+Console.WriteLine("Dados da conta:");
+Console.WriteLine(conta);
+
+Console.WriteLine();
+Console.Write("Entre um valor para depósito: ");
+double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Deposito(quantia);
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(conta);
+
+Console.WriteLine();
+Console.Write("Entre um valor para saque: ");
+quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Saque(quantia);
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(conta);
