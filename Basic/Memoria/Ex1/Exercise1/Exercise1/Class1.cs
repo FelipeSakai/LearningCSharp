1730fd8c6b9ee50a4a591b7a41dba0d380cd9199
Felipe <felipesakaiipolito@gmail.com>
2024-09-27 15:10:24 -0400

    another exercise, memory with vector

---
+++ Basic/Memoria/Ex1/Exercise1/Exercise1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Estudante
+    {
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        
+        public Estudante(string nome,string email) {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", "+ Email;
+        }
+    }
+}
---
+++ Basic/Memoria/Ex1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Exercise1;
+using System;
+
+
+class Program { 
+static void Main(string[] args)
+{
+    Estudante[] vect = new Estudante[10];
+
+    Console.WriteLine("Quantos quartos serao alugados ? ");
+    int n = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Aluguel #{i}: ");
+        Console.WriteLine("Nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Email: ");
+        string email = Console.ReadLine();
+        Console.WriteLine("Quarto: ");
+        int quarto = int.Parse(Console.ReadLine());
+        vect[quarto] = new Estudante(nome, email);
+
+    }
+    Console.WriteLine();
+    Console.WriteLine("Quartos Ocupados: ");
+    for (int i = 0; i < 10; i++)
+    {
+        if (vect[i] != null)
+        {
+            Console.WriteLine(i + ": " + vect[i]);
+        }
+    }
+
+}
+}
