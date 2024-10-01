5e23c862813c457be897f0c21ce08b2733dfc6b2
Felipe <felipesakaiipolito@gmail.com>
2024-10-01 17:23:46 -0400

    Some List Exercise

---
+++ Basic/Memoria/List/Training/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp1;
+
+Console.WriteLine("Enter the number of students in the class: ");
+int n = int.Parse(Console.ReadLine());
+
+List<Student> list = new List<Student>();
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("Student #" +i + ":");
+    Console.Write("ID: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Grade: ");
+    int grade = int.Parse(Console.ReadLine());
+    list.Add(new Student(id,name,grade));
+    Console.WriteLine();
+}
+
+Console.WriteLine("Enter the id who made the best homework: ");
+int searchId= int.Parse(Console.ReadLine());
+
+Student std = list.Find(x => x.Id == searchId);
+
+if (std != null)
+{
+    Console.WriteLine("Enter the grade of the homework: ");
+    int work = int.Parse(Console.ReadLine());
+    std.SchoolWork(work);
+}
+else
+{
+    Console.WriteLine("This Id is wrong!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Update the grade of the students");
+foreach (Student obj in list)
+{
+    Console.WriteLine(obj);
+}
---
+++ Basic/Memoria/List/Training/ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Grade { get; set; }
+
+
+        public Student(int id, string name, int grade)
+        {
+            Id = id;
+            Name = name;
+            Grade = grade;
+        }
+
+        public void SchoolWork(int Work) => Grade = Grade + Work;
+
+        public override string ToString()
+        {
+            return Id
+                +", "
+                +Name
+                +", "
+                +Grade.ToString();
+        }
+    }
+}
