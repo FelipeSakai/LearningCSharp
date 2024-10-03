8fd1933300a3df93216d235159d157d6d17760cf
Felipe <felipesakaiipolito@gmail.com>
2024-10-03 10:47:43 -0400

    Learning Enumeration

---
+++ Intermediary/Enum/Basic/Lesson1/Lesson1/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson1.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Delivered = 2,
+        Receveid = 3
+
+    }
+}
---
+++ Intermediary/Enum/Basic/Lesson1/Lesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lesson1.Entities;
+using Lesson1.Entities.Enum;
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+
+}
