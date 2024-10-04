b68130830e7869ff0c72569223b2ad0997e99dd0
Felipe <felipesakaiipolito@gmail.com>
2024-10-04 17:21:18 -0400

    Unfinished Project

---
+++ Intermediary/Projects/ShopProject/ShopProject/ShopProject/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopProject.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        Pending_Payment = 0,
+        Processing = 1 ,
+        Shipped = 2, 
+        Delivered = 3
+
+    }
+}
---
+++ Intermediary/Projects/ShopProject/ShopProject/ShopProject/Entities/Order.cs
@@ -0,0 +1,52 @@
+﻿using ShopProject.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void removeItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem item in Items)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+    }
+}
