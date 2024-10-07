120265435467232a8653baaaf2d76625f3fc32d5
Felipe <felipesakaiipolito@gmail.com>
2024-10-07 11:02:46 -0400

    Using Inheritance in CSharp

---
+++ Intermediary/Heritage/Firstime/Heritage/Heritage/Entities/BussinesAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Entities
+{
+    internal class BussinesAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+
+        public BussinesAccount() { }
+
+        public BussinesAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void loan(double ammout)
+        {
+            if(ammout <= LoanLimit)
+            Balance += ammout;
+        }
+    }
+}
---
+++ Intermediary/Heritage/Firstime/Heritage/Heritage/Program.cs
@@ -0,0 +1,12 @@
+﻿using Heritage.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BussinesAccount account = new BussinesAccount(8010,"Tonho Culher",100.00,500.00);
+
+
+        Console.WriteLine(account.Balance);
+    }
+}
