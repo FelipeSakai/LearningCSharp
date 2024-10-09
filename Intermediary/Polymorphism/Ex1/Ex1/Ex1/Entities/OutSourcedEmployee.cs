2358d9c755d932723600afd675cb47e2f1f32a2a
Felipe <felipesakaiipolito@gmail.com>
2024-10-09 16:39:25 -0400

    Exercises

---
+++ Intermediary/Polymorphism/Ex1/Ex1/Ex1/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1.Entities
+{
+    internal class OutSourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourcedEmployee()
+        {
+
+        }
+
+        public OutSourcedEmployee(double additionalCharge, string name, int hours, double valuePerHour):base(name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
