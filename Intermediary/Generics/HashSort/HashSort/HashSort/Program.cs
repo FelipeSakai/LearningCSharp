eec643645ce54016f913f30cbd87e39f73646262
Felipe <felipesakaiipolito@gmail.com>
2024-10-23 17:09:33 -0400

    hash and sort

---
+++ Intermediary/Generics/HashSort/HashSort/HashSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SortedSet<int> a = new SortedSet<int>() { 0, 2, 5, 7, 9 };
+        SortedSet<int> b = new SortedSet<int>() { 3, 6, 8, 10 };
+
+        SortedSet<int> c = new SortedSet<int>(a);
+        c.UnionWith(b);
+        printCollection(c);
+
+        SortedSet<int> d = new SortedSet<int>(a);
+        d.IntersectWith(b);
+        printCollection(d);
+
+        SortedSet<int> e = new SortedSet<int>(a);
+        e.ExceptWith(b);
+        printCollection(e);
+    }
+
+    static void printCollection<T>(IEnumerable<T> collection)
+    {
+        foreach (T obj in collection)
+        {
+            Console.Write(obj + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Intermediary/Generics/Test/TestGenerics/TestGenerics/PrintService.cs
@@ -0,0 +1,39 @@
+﻿namespace TestGenerics
+{
+    internal class PrintService <T>
+    {
+        private T[] values = new T[10];
+        private int count = 0;  
+
+        public void AddValue(T value) {
+            if(count == 10)
+            {
+                throw new InvalidOperationException("PS is full");
+            }
+            values[count] = value;
+            count++;
+        }
+        public T First()
+        {
+            if (count == 00)
+            {
+                throw new InvalidOperationException("PS is empty");
+            }
+            return values[0];
+        }
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < count - 1; i++)
+            {
+                Console.Write(values[i] + ", ");
+            }
+            if (count > 0)
+            {
+                Console.Write(values[count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+
+    }
+}
