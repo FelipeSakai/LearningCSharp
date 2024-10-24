899d4d2f5d04627c8a0aedb895ff531c2d6ed079
Felipe <felipesakaiipolito@gmail.com>
2024-10-24 16:58:02 -0400

    Dictionary

---
+++ Intermediary/Generics/HashSort/Ex/ex2/Ex2/Ex2/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> groupA = new HashSet<int>();
+        HashSet<int> groupB = new HashSet<int>();
+        HashSet<int> groupC = new HashSet<int>();
+
+        Console.Write("How many students in the Group A: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int cod = int.Parse(Console.ReadLine());
+            groupA.Add(cod);
+        }
+
+        Console.Write("How many students in the Group B: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int cod = int.Parse(Console.ReadLine());
+            groupB.Add(cod);
+        }
+
+        Console.Write("How many students in the Group C: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int cod = int.Parse(Console.ReadLine());
+            groupC.Add(cod);
+        }
+
+        HashSet<int> all = new HashSet<int>(groupA);
+        all.UnionWith(groupB);
+        all.UnionWith(groupC);
+        Console.WriteLine("Total Students: "+ all.Count);
+
+    }
+}
